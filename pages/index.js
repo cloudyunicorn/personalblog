eec7cb13e034ede7d967a964c9bdb9975609fa72
Rajat Singh <s.rajat55@gmail.com>
2022-01-04 05:06:49 +0530

    changed brand name font correction

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ export default function Home({ posts }) {
           href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        ></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
