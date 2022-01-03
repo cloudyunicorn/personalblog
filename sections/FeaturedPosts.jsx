9acb6e0922bb3f4369f650ddc17b0b03590b531e
Rajat Singh <s.rajat55@gmail.com>
2022-01-04 01:28:53 +0530

    corrected arrow carousel

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -36,10 +36,10 @@ const FeaturedPosts = () => {
   }, []);
 
   const customLeftArrow = (
-    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full content-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-white w-full"
+        className="h-6 w-6 text-white w-full inline-block"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -55,10 +55,10 @@ const FeaturedPosts = () => {
   );
 
   const customRightArrow = (
-    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full content-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-white w-full"
+        className="h-6 w-6 text-white w-full inline-block"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
